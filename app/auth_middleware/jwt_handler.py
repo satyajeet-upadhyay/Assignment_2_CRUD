a56df3630db0fdfe7a623ab4c4aab4453c52635e
Sourav23125104 <sourav.kumar@jktech.com>
2025-07-11 16:34:24 +0530

    Add : .env file

--- app/auth_middleware/jwt_handler.py
+++ app/auth_middleware/jwt_handler.py
@@ -1,8 +1,12 @@
+import os
 from datetime import datetime, timedelta
 from jose import jwt
+from dotenv import load_dotenv
 
-SECRET_KEY = ""  
-ALGORITHM = "HS256"
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
 TOKEN_EXPIRE_MIN = 30
 
 def create_access_token(data: dict):
